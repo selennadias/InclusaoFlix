c29bd3e57edb4ce952d4fa8ad047a8461ef4ebe1
selennadias <selennadias@gmail.com>
2020-11-20 00:18:55 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import dadosIniciais from './data/dados.json'
 import Banner from './components/Banner'
-
+import Carousel from "./components/Carousel"
 function App() {
   return (
     <div>
@@ -10,6 +10,21 @@ function App() {
        videoTitle={dadosIniciais.categorias[0].videos[0].titulo} 
        url={dadosIniciais.categorias[0].videos[0].url} 
        videoDescription={"Projeto InclusaoFLIX"} /> 
+      
+     <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+        />
+        <Carousel
+        category={dadosIniciais.categorias[1]}
+        />
+        <Carousel
+        category={dadosIniciais.categorias[2]}
+        />
+        <Carousel
+        category={dadosIniciais.categorias[3]}
+     />
+
    </div> 
 ); 
 } 

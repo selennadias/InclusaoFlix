cae3cef0cb5df8b64f5b371b353c0927d6774e66
selennadias <selennadias@gmail.com>
2020-11-29 20:49:38 -0300

    Atualização

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./pages/Home";
+import Categorias from "./pages/Categorias";
+import Videos from "./pages/Videos";
 import "./index.css";
-
-
+import { Redirect , Switch, Route, BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
-  <React.StrictMode>
-    
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <BrowserRouter>
+ <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/categorias" component={Categorias}/>
+      <Route path="/videos" component={Videos}/>
+ </Switch>
+ </BrowserRouter>,
+ document.getElementById("root")
 );
 
--- src/pages/Categorias/index.js
+++ src/pages/Categorias/index.js
@@ -2,25 +2,73 @@ import React, {useState, useEffect} from 'react';
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { useForm } from 'react-hook-form';
+import { Link, useHistory } from 'react-router-dom';
+import "./index.css"
+import ModalDialog from '../../components/Modal';
 function CadastroCategoria() {
- const { register, handleSubmit, errors } = useForm();
- return (
- <div>
- <Header />
- <h1> Cadastro Categoria </h1>
- <form onSubmit={handleSubmit()} className="form-group">
- <input type="text" className="formcontrol" placeholder="Título" name="titulo" ref={register({required: true})} />
- <br />
- <input type="text" className="formcontrol" placeholder="Descrição Extra" name="descricao" ref={register({required: true})} />
- <br />
- <input type="text" className="formcontrol" placeholder="Link Extra" name="url" ref={register({required: true})} />
- <br />
- <input type="color" className="formcontrol" placeholder="Cor" name="cor" ref={register({required: true})} />
- <br />
- <input type="submit" className="btn btn-primary btnlg" value="Salvar"/>
- </form>
- <Footer />
- </div>
- )
+    const { register, handleSubmit, errors } = useForm();
+    const history = useHistory();
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {setOpen(true);};
+    const handleNo = () => {  
+      setOpen(false); 
+      history.push('/');
+    };
+    const handleYes = () => {
+      alert("Clicou em sim");
+      setOpen(false);
+    };
+
+
+
+    function onSubmit(dados) {
+      console.log("Dados:", dados);
+      fetch(`http://localhost:8080/categorias`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dados),
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .catch((error) => console.error("Error:", error))
+        .then((response) => console.log("Success:", JSON.stringify(response)))
+        .then(handleClickOpen());     
+        console.log(dados);
+    }
+    return (
+        <div className="corpo">
+        <Header />
+         <h1>  Cadastro Categoria </h1>
+         <br />
+         <form onSubmit={handleSubmit(onSubmit)} className="form-group" className="container">
+         <input type="text" className="form-control" placeholder="Título " name="titulo" ref={register({required: true})} />
+          <br />
+          <input type="text" className="form-control" placeholder="Descrição Extra" name="descricao" ref={register({required: true})} />
+          <br />
+          <input type="text" className="form-control" placeholder="Link Extra" name="url" ref={register({required: true})} />
+          <br />
+          <input type="color" className="form-control" placeholder="Cor" name="cor" ref={register({required: true})} />
+          <br />           
+           <input type="submit" className="btn btn-primary btn-lg" value="Salvar"/>
+         </form>
+         <br /> 
+         <ModalDialog 
+             open = {open}
+             handleClose = {handleNo}
+             titulo = 'Cadastrado com sucesso'
+             texto = 'Deseja fazer outro cadastro?'
+             handleSim = {handleYes}
+             handleNao = {handleNo}
+          />
+          <Link to="/">
+                Ir para home
+          </Link>
+         <Footer />
+         </div>
+         )
 };
+
 export default CadastroCategoria;

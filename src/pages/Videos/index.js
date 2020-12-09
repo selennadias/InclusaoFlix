d1ba8ba1186aa9ed7adf18f1fa123f34e51c6286
selennadias <selennadias@gmail.com>
2020-12-08 22:51:15 -0300

    Atualização Aula

--- src/pages/Videos/index.js
+++ src/pages/Videos/index.js
@@ -4,10 +4,54 @@ import { Link, useHistory } from "react-router-dom";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import "./index.css"
+import repository from "../../Repository.js"
+import ModalDialog from '../../components/Modal';
 
 function CadastroVideo() {
     const { register, handleSubmit, errors } = useForm();
     const [categorias, setCategorias] = useState([]);
+
+    const history = useHistory();
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {setOpen(true);};
+    const handleNo = () => {  
+      setOpen(false); 
+      history.push('/');
+    };
+    const handleYes = () => {
+      alert("Clicou em sim");
+      setOpen(false);
+    };
+
+
+    useEffect(() => {
+      repository.getCategorias().then(async (categorias) => {
+       await setCategorias(categorias);
+      });
+      }, []);
+
+      function onSubmit(dados) {
+        console.log("Dados:", dados);
+        fetch(`http://localhost:8080/videos`, {
+        method: "post",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+        ...dados,
+        categoriaId: Number(dados.categoriaId),
+        }),
+        })
+        .then(function (response) {
+        return response.json();
+        })
+        .then(() => {
+        console.log("Cadastrou com sucesso!");
+        })
+        .catch((error) => console.error("Error:", error))
+        .then((response) => console.log("Success:", JSON.stringify(response)))
+        .then(handleClickOpen());   
+        }
 
     return (
          <div>
@@ -16,7 +60,7 @@ function CadastroVideo() {
             
             <form className="container"
               id="video-form"
-              onSubmit={handleSubmit()}
+              onSubmit={handleSubmit(onSubmit)}
               
             >
             <h1> Cadastro de Vídeos </h1>
@@ -63,8 +107,21 @@ function CadastroVideo() {
                 value="Salvar"
               />
             </form>
+            <br />
             <Link to="/categorias">Cadastrar Categoria </Link>
             </div>
+            <ModalDialog 
+             open = {open}
+             handleClose = {handleNo}
+             titulo = 'Cadastrado com sucesso'
+             texto = 'Deseja fazer outro cadastro?'
+             handleSim = {handleYes}
+             handleNao = {handleNo}
+          />
+          <Link to="/">
+                Ir para home
+          </Link>
+
             <Footer />
             </div>
 
